b6a51d8fa2652d0a24f7442e01e265242795b415
harunmioc <harun.mioc@gmail.com>
2023-10-31 13:01:54 +0100

    zadatak 2 lab 2

---
+++ LV2-Z2/Osoba/src/main/java/org/example/Osoba.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Osoba {
+    private String ime;
+    private String prezime;
+
+
+    public String getIme() {
+        return ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Osoba(String ime, String prezime) {
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "ime='" + ime + '\'' +
+                ", prezime='" + prezime + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Osoba osoba = (Osoba) o;
+        return Objects.equals(ime, osoba.ime) && Objects.equals(prezime, osoba.prezime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ime, prezime);
+    }
+}
