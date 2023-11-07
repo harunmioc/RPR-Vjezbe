04a8390614e2e4cd1634a06419d3a489bdec7f97
harunmioc <harun.mioc@gmail.com>
2023-11-07 21:43:34 +0100

    obrisan folder

--- lv3-z1/src/main/java/ba/unsa/etf/rpr/lv3/z1/BrojException.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.rpr.lv3.z1;
-
-public class BrojException extends RuntimeException {
-    public BrojException(String msg){
-        super(msg);
-    }
-}
--- lv3-z1/src/main/java/ba/unsa/etf/rpr/lv3/z1/TelefonskiBroj.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rpr.lv3.z1;
-
-public abstract class TelefonskiBroj {
-    public abstract String ispisi();
-    public abstract int hashCode();
-}
